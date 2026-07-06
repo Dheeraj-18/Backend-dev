f02e4f5dec761f615dcfbbca3d18b0fd7cbf476c
think2code <dheerajsaraswat03@gmail.com>
2026-07-06 18:09:38 +0530

    configure for response and write utilites

--- src/index.js
+++ src/index.js
@@ -6,8 +6,43 @@ import connectDB from './db/index.js';
 
 dotenv.config({ path: './env' });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MONGODB Connection FAILED !!!!!', err);
+  });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 /*
 import express from 'express';
 
